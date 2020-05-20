00f6d3faddef1dd167b49229b8a29bcda1fbbae9
demetrius <d.natxa@gmail.com>
2020-05-20 15:02:51 +0300

    logging to email, log handler selector in settings.ini

--- wss/data/log.py
+++ wss/data/log.py
@@ -1,18 +1,37 @@
 import logging
+from logging.handlers import SMTPHandler
+
+from wss.data.config import config
 
 
 def logger_setup(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler('wss/wss.log')
-
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
-    console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
 
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
+    if config['LOGGING']['console'] == '1':
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+    if config['LOGGING']['file'] == '1':
+        file_handler = logging.FileHandler('wss/wss.log')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    if config['LOGGING']['email'] == '1':
+        mail_handler = SMTPHandler(
+            mailhost=(config['EMAIL']['host'], config['EMAIL']['port']),
+            fromaddr=config['EMAIL']['from'],
+            toaddrs=config['EMAIL']['recipient'],
+            subject='WSS - Weather Service Statistics - Logging: CRITICAL',
+            credentials=(config['EMAIL']['login'], config['EMAIL']['password']),
+            secure=()
+        )
+        mail_handler.setFormatter(formatter)
+        # send to mail only critical logs
+        mail_handler.setLevel(logging.CRITICAL)
+        logger.addHandler(mail_handler)
 
     return logger
