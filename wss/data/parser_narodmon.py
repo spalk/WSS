11fd38fa1fb8b7dd37994b84ee662f27530dc8a7
demetrius <d.natxa@gmail.com>
2020-05-25 23:52:27 +0300

    pressure sensor added

--- wss/data/parser_narodmon.py
+++ wss/data/parser_narodmon.py
@@ -12,15 +12,22 @@ class ParserNarodmon():
     def feed(self, content_json):
         self.content = content_json
         content_dic = json.loads(content_json)
-        # {"sensors":[{"id":2620,"type":1,"value":11.9,"time":1590437910,"changed":1590437910,"trend":0}]}
-        self.value_t = content_dic['sensors'][0]['value']
-        self.dt_unix = content_dic['sensors'][0]['time']
+        # http://narodmon.ru/api/sensorsValues?sensors=2620,2621&uuid=53ad04c130d27cdce00a52ffc9570c30&api_key=Fv2PUCB4UKGGe
+        self.t_value = content_dic['sensors'][0]['value']
+        self.t_dt_unix = content_dic['sensors'][0]['time']
+        self.p_value = content_dic['sensors'][1]['value']
+        self.p_dt_unix = content_dic['sensors'][1]['time']
 
     def set_data_types(self):
         """Clearing data and converting from strings to datetime and int"""
-        value = float(self.value_t)
-        dt = datetime.datetime.fromtimestamp(int(self.dt_unix))
-        self.data = [{'datetime': dt, 'parameter': 't', 'value': value}]
+        t_value = float(self.t_value)
+        t_dt = datetime.datetime.fromtimestamp(int(self.t_dt_unix))
+        p_value = float(self.p_value)
+        p_dt = datetime.datetime.fromtimestamp(int(self.p_dt_unix))
+        self.data = [
+            {'datetime': t_dt, 'parameter': 't', 'value': t_value},
+            {'datetime': p_dt, 'parameter': 'p', 'value': p_value}
+        ]
 
     def get_data(self):
         return self.data
