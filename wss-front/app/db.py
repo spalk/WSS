ab4aa63d26d5ef495f12f4689a8ca575532984df
demetrius <d.natxa@gmail.com>
2020-06-06 22:59:42 +0300

    pressure page is ready

--- wss-front/app/db.py
+++ wss-front/app/db.py
@@ -55,41 +55,6 @@ class DB:
         data_sorted = sorted(data, key=itemgetter('date'))
         return data_sorted
 
-    def get_data_for_chart_(self, service):
-        # SELECT * FROM session WHERE stop_time > (SELECT DATETIME('now', '-7 day'))
-        history_days = config['DB']['history_days']
-        forecast_days = config['DB']['forecast_days']
-        history_dt_req = '''(SELECT DATETIME('now', '-%s day'))''' % history_days
-        forecast_dt_req = '''(SELECT DATETIME('now', '+%s day'))''' % forecast_days
-        req = ''' SELECT * 
-            FROM temperature 
-            WHERE service = ?
-            AND datetime > %s
-            AND datetime < %s''' % (history_dt_req, forecast_dt_req)
-        self.c.execute(req, (service,))
-        rows = self.c.fetchall()
-
-        data = []
-        used_keys = []
-        for r in rows:
-            dt = r[2]
-            if dt in used_keys:
-                for dic in data:
-                    if dic['date'] == dt:
-                        if dic['close'] < r[3]:
-                            dic['close'] = r[3]
-                        elif dic['open'] > r[3]:
-                            dic['open'] = r[3]
-            else:
-                data.append(
-                    {'date': dt,
-                     'open': r[3],
-                     'close': r[3]}
-                )
-                used_keys.append(dt)
-        data_sorted = sorted(data, key=itemgetter('date'))
-        return data_sorted
-
     def get_max(self, dt, parameter, service):
         req = '''SELECT max("value") 
                  FROM %s 
--- wss-front/app/routes.py
+++ wss-front/app/routes.py
@@ -16,3 +16,16 @@ def index():
                            data_yandex=data_yandex,
                            data_rp5=data_rp5,
                            data_narodmon=data_narodmon)
+
+@app.route('/pressure')
+def pressure():
+    db_inst = db.DB()
+    data_yandex = db_inst.get_data_for_chart('yandex', parameter='pressure')
+    data_rp5 = db_inst.get_data_for_chart('rp5', parameter='pressure')
+    data_narodmon = db_inst.get_data_for_chart('narodmon', parameter='pressure')
+    db_inst.db_close()
+
+    return render_template('pressure.html',
+                           data_yandex=data_yandex,
+                           data_rp5=data_rp5,
+                           data_narodmon=data_narodmon)
