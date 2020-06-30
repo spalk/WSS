03efb22460e073721de9f50841460551e0ca7f75
demetrius <d.natxa@gmail.com>
2020-07-01 00:53:33 +0300

    bug fix

--- wss-front/app/static/wss.js
+++ wss-front/app/static/wss.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var nav = document.getElementById('nav'),
     anchor = nav.getElementsByTagName('a'),
     current = window.location.pathname.split('/')[1];
-    for (var i = 0; i < anchor.length; i++) {vb
+    for (var i = 0; i < anchor.length; i++) {
         if(anchor[i].getAttribute("href") == current) {
             anchor[i].className = "pagename current";
         }
