3375831c52bb29c812f78fbb09a1951a98123265
demetrius <d.natxa@gmail.com>
2020-07-01 22:32:41 +0300

    Sensor DS18B20 added to South Balcony wemos to compare with bme280 temperature data

--- sensors/wemos_bme280/wemos_bme280.ino
+++ sensors/wemos_bme280/wemos_bme280.ino
@@ -1,7 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <Adafruit_BME280.h>
+#include <OneWire.h>
 
 Adafruit_BME280 bme; // I2C
+
+// OneWire DS18B20
+OneWire  ds(D4);
 
 // Replace with your network details
 const char* ssid = "Keenetic-9275";
@@ -13,7 +17,8 @@ float h, t, p;
 char temperatureString[6];
 char humidityString[6];
 char pressureString[7];
-String req, par;
+char temperatureDSString[6];
+String req, par, ds_sensor_name, ds_sensor_key;
 
 
 WiFiClient nmClient;
@@ -53,6 +58,7 @@ void loop() {
     dtostrf(getTemperature(), 5, 2, temperatureString);
     dtostrf(getPressure(), 6, 2, pressureString);
     dtostrf(getHumidity(), 5, 2, humidityString);
+    dtostrf(getTemperatureDS(), 5, 2, temperatureDSString);
 
     // send t
     Serial.println();
@@ -77,6 +83,16 @@ void loop() {
     Serial.println("*** Sending humidity ***");
     par = 'h';
     req = create_url(par, humidityString);
+    Serial.println(req);
+    send_data(req);
+
+    // send t DS
+    Serial.println();
+    Serial.println("*** Sending temperature from DS sensor ***");
+    par = 't';
+    ds_sensor_name = "wemos_south_balcony_DS18B20";
+    ds_sensor_key = "ac56f7";
+    req = create_url_(par, temperatureDSString, ds_sensor_name, ds_sensor_key);
     Serial.println(req);
     send_data(req);
     delay(600000);
@@ -133,6 +149,21 @@ String create_url(String p, String v){
     return url;
 }
 
+String create_url_(String p, String v, String s, String k){
+    // create a URI for the request
+    // http://localhost:5000/sensor-data?sensorname=wemos_south_balcony&parameter=t&value=24&key=9b33a5
+    String url = "/sensor-data";
+    url += "?sensorname=";
+    url += s;
+    url += "&parameter=";
+    url += p;
+    url += "&value=";
+    url += v;
+    url += "&key=";
+    url += k;
+    return url;
+}
+
 void send_data (String url){
     // sending data to server
     Serial.print("Connecting to: ");
@@ -167,4 +198,76 @@ void send_data (String url){
     }
     Serial.println();
     Serial.println("Closing connection");
+}
+
+float getTemperatureDS (){
+    byte i;
+    byte present = 0;
+    byte type_s;
+    byte data[12];
+    byte addr[8];
+    float celsius;
+
+    if ( !ds.search(addr))
+    {
+        ds.reset_search();
+        delay(250);
+        return 0;
+    }
+
+    if (OneWire::crc8(addr, 7) != addr[7])
+    {
+        Serial.println("CRC is not valid!");
+        return 0;
+    }
+
+    // the first ROM byte indicates which chip
+    switch (addr[0])
+    {
+    case 0x10:
+        type_s = 1;
+        break;
+    case 0x28:
+        type_s = 0;
+        break;
+    case 0x22:
+        type_s = 0;
+        break;
+    default:
+        Serial.println("Device is not a DS18x20 family device.");
+        return 0;
+    }
+
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+    delay(1000);
+    present = ds.reset();
+    ds.select(addr);
+    ds.write(0xBE);         // Read Scratchpad
+
+    for ( i = 0; i < 9; i++)
+    {
+        data[i] = ds.read();
+    }
+
+    // Convert the data to actual temperature
+    int16_t raw = (data[1] << 8) | data[0];
+    if (type_s)
+    {
+        raw = raw << 3; // 9 bit resolution default
+        if (data[7] == 0x10)
+        {
+            raw = (raw & 0xFFF0) + 12 - data[6];
+        }
+    }
+    else
+    {
+        byte cfg = (data[4] & 0x60);
+        if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+        else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+        else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    }
+    celsius = (float)raw / 16.0;
+    return celsius;
 }
